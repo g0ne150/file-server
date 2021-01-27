951e11831d55f0029eedd2f9bc699f6e75f8c7cd
Zapan Gao <g0ne150@hotmail.com>
2021-01-27 19:13:23 +0800

    init project

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import Koa from "koa"
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = "Hello World"
+})
+
+app.listen(8080, () => {
+    console.log("Server is running: http://localhost:8080")
+})

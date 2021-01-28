0c2d013c97d2571f9e6379d3983012260a4c9873
Zapan Gao <g0ne150@hotmail.com>
2021-01-28 09:41:09 +0800

    add koa-static;

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import Koa from "koa"
+import staticServe from "koa-static"
+
+
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = "Hello World"
+// 错误处理
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (e) {
+        console.error(`error: `, e)
+        if (e instanceof Error) {
+            ctx.body = {
+                code: 500,
+                msg: e.message
+            };
+        }
+    }
 })
+
+// 静态资源
+app.use(staticServe(`${__dirname}/../public`))
 
 app.listen(8080, () => {
     console.log("Server is running: http://localhost:8080")

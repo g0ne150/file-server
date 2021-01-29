3d47e16cff6c5c7db0ed6a23985828fba01bbe36
Zapan Gao <g0ne150@hotmail.com>
2021-01-29 10:13:37 +0800

    add comments for config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,14 @@
+/**
+ * Server listen port
+ */
 export const PORT = 8080
 
+/**
+ * SQLite3 database file path
+ */
 export const SQLITE3_DATABASE_FILE_PATH = `${__dirname}/../sql/file-server.db`
 
+/**
+ * Upload file storage localtion
+ */
 export const FILE_STORAGE_PATH = ""
